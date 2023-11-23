7d6afe021c2cc835b1979e2fff160d02bb8f91ca
Victor Ormeño <20100835@ue.edu.pe>
2023-11-23 12:34:32 -0500

    InteraccionDTO and Usuario DTO

---
+++ UESAN.Jobs.Core/DTO/InteraccionDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UESAN.Proyecto.Core.DTO
+{
+	public class InteraccionDTO
+	{
+		public int IdInteraccion { get; set; }
+
+		public int? IdUsuario { get; set; }
+
+		public int? IdEvento { get; set; }
+
+		public string? Tipo { get; set; }
+
+		public string? Descripcion { get; set; }
+
+	}
+
+	public class InteraccionInsertDTO
+	{
+		public int? IdUsuario { get; set; }
+
+		public int? IdEvento { get; set; }
+
+		public string? Tipo { get; set; }
+
+		public string? Descripcion { get; set; }
+
+	}
+}
---
+++ UESAN.Jobs.Core/DTO/UsuarioDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UESAN.Proyecto.Core.DTO
+{
+	public class UsuarioDTO
+	{
+		public int IdUsuario { get; set; }
+
+		public string? Correo { get; set; }
+
+		public string? Nombre { get; set; }
+
+		public string? Area { get; set; }
+
+		public string? Tipo { get; set; }
+
+		public string? Estado { get; set; }
+	}
+
+	public class UsuarioCreateDTO
+	{
+		public string? Correo { get; set; }
+
+		public string? Nombre { get; set; }
+
+		public string? Area { get; set; }
+
+	}
+}
