ed8170bd6d114d3c001d37ec62d44ef166e35b58
Victor Ormeño <20100835@ue.edu.pe>
2023-11-23 12:37:09 -0500

    InterfacesRepository: Evento, interacciones, usuario

---
+++ UESAN.Jobs.Core/InterfacesRepository/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using UESAN.Proyecto.Core.entities;
+
+namespace UESAN.proyecto.Infrastructure.repository
+{
+	public interface IUsuarioRepository
+	{
+		Task<bool> delete(int id);
+		Task<IEnumerable<Usuarios>> getAll();
+		Task<int> Insert(Usuarios u);
+		Task<bool> IsEmailRegistered(string email);
+		Task<Usuarios> SigIn(string username);
+		Task<bool> update(Usuarios u);
+	}
+}
