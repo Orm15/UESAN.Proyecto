d27627a3fcb4893cbbd1882138daf5648bdddadc
Victor Ormeño <20100835@ue.edu.pe>
2023-12-08 23:37:22 -0500

    UsuarioController

--- UESAN.Jobs.API/Program.cs
+++ UESAN.Jobs.API/Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
 using UESAN.proyecto.Infrastructure.Models;
-using UESAN.Proyecto.Core.entities;
+using UESAN.proyecto.Infrastructure.repository;
+using UESAN.Proyecto.Core.InterfacesRepository;
+using UESAN.Proyecto.Core.InterfacesServices;
+using UESAN.Proyecto.Core.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +18,25 @@ builder
 	   .AddDbContext<OrdenesEventosContext>
 	   (options => options.UseSqlServer(connectionString));
 
-builder.Services.AddControllers();
+//Aqui se  registran las clases:
+builder.Services.AddTransient<IUsuarioRepository,UsuarioRepository>();
+builder.Services.AddTransient<IUsuarioServices,UsuarioServices>();
+
+
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+	options
+		.JsonSerializerOptions
+		.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 var app = builder.Build();
 
