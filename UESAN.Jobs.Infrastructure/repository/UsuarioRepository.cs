ca5629bb74b28d4639ef82d7affb133c4330724c
Victor Ormeño <20100835@ue.edu.pe>
2023-12-08 23:35:19 -0500

    UsuarioRepository

--- UESAN.Jobs.Infrastructure/repository/UsuarioRepository.cs
+++ UESAN.Jobs.Infrastructure/repository/UsuarioRepository.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UESAN.proyecto.Infrastructure.Models;
 using UESAN.Proyecto.Core.entities;
 using UESAN.Proyecto.Core.InterfacesRepository;
+using UESAN.Proyecto.Core.Utilidades;
 
 namespace UESAN.proyecto.Infrastructure.repository
 {
@@ -19,24 +21,50 @@ namespace UESAN.proyecto.Infrastructure.repository
 		{
 			_context = context;
 		}
-		//Funcion del admin
-		public async Task<IEnumerable<Usuarios>> getAll()
+		//Funcion del admin, puede llamar por medio de un filtro
+		public async Task<IEnumerable<Usuarios>> getAll(string estado)
 		{
+			if (estado == "todos")
+			{
+				return await _context.Usuarios.ToListAsync();
+			}
+			else { 
+				var usu =  await _context.Usuarios.Where(x => x.Estado == estado).ToListAsync();
+				if (usu.Any())
+				{
+					return usu;
+				}
+				else {
+					return null;
+				}
+			}
 
-			return await _context.Usuarios.ToListAsync();
 		}
 
-		public async Task<int> Insert(Usuarios u)
+		public async Task<Usuarios> getById(int id)
+		{
+			var usu = await _context.Usuarios.Where(x => x.IdUsuario == id).FirstOrDefaultAsync();
+			if(usu == null)
+			{
+				return null;
+			}
+			else
+			{
+				return usu;
+			}
+		}
+
+		public async Task<Usuarios> Insert(Usuarios u)
 		{
 			await _context.Usuarios.AddAsync(u);
 			int rows = await _context.SaveChangesAsync();
 			if (rows > 0)
 			{
-				return u.IdUsuario;
+				return u;
 			}
 			else
 			{
-				return 0;
+				return null;
 			}
 		}
 
@@ -66,10 +94,10 @@ namespace UESAN.proyecto.Infrastructure.repository
 				.Usuarios
 				.Where(x => x.Correo == email).AnyAsync();
 		}
-
-		public async Task<Usuarios> SigIn(string username)
+		//SIGIN SIN SALT
+		public async Task<Usuarios> SigIn(string correo, string contra)
 		{
-			var u = await _context.Usuarios.Where(x => x.Correo == username && x.Estado == "activo")
+			var u = await _context.Usuarios.Where(x => x.Correo == correo && x.Contra == contra && x.Estado == "activo")
 				.FirstOrDefaultAsync();
 			if (u == null)
 			{
@@ -80,6 +108,38 @@ namespace UESAN.proyecto.Infrastructure.repository
 				return u;
 			}
 		}
+		//SIGIN CON SALT.
+
+		public async Task<Usuarios> SigInSalt(string correo, string contra)
+		{
+			var user = await _context.Usuarios
+				.Where(x => x.Correo == correo && x.Estado == "activo")
+				.FirstOrDefaultAsync();
+
+			if (user != null && VerifyPassword(contra, user.Contra,user.Salt))
+			{
+				return user;
+			}
+
+			return null;
+		}
+
+		private bool VerifyPassword(string PasswordIngresado, string PasswordAlmacenado,string salt)
+		{
+			// Aquí extraemos la sal del hash almacenado
+			string storedSalt = salt;
+			// Calculamos el hash de la contraseña proporcionada con la sal almacenada
+			string hashedPassword = SecurityHelperUtilidades.HashPassword(PasswordIngresado.Trim(), storedSalt.Trim());
+			// Comparamos los resultados
+			Console.WriteLine($"PasswordAlmacenado: {PasswordAlmacenado}");
+
+			return PasswordAlmacenado.Trim() == hashedPassword.Trim();
+		}
+
+		
+
+
+
 
 
 
