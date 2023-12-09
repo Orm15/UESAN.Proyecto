7d645950f11b7b04feebb4538a5923a630fa634b
Victor Ormeño <20100835@ue.edu.pe>
2023-12-08 23:36:03 -0500

    Utilidades

---
+++ UESAN.Jobs.Core/Utilidades/SecurityHelperUtilidades.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UESAN.Proyecto.Core.Utilidades
+{
+	public class SecurityHelperUtilidades
+	{
+		public static string HashPassword(string password, string salt)
+		{
+			using (var sha256 = SHA256.Create())
+			{
+				var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password + salt));
+				return Convert.ToBase64String(hashedBytes);
+			}
+		}
+
+		public static string GenerateSalt()
+		{
+			byte[] saltBytes = new byte[8];
+			using (var rng = new RNGCryptoServiceProvider())
+			{
+				rng.GetBytes(saltBytes);
+			}
+			return Convert.ToBase64String(saltBytes);
+		}
+
+		public static (string Password, string Salt) SetPassword(string password)
+		{
+			string Salt, Contra;
+			// Generar una nueva sal y calcular el hash de la contraseña
+			Salt = SecurityHelperUtilidades.GenerateSalt();
+			Contra = SecurityHelperUtilidades.HashPassword(password, Salt);
+			return  (Contra,Salt);
+		}
+
+
+
+	}
+}
