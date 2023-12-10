6822757b17bc061e6840a813c736615af7948dbc
Victor Ormeño <20100835@ue.edu.pe>
2023-12-10 17:07:05 -0500

    servicioRepository avance 1

--- UESAN.Jobs.Infrastructure/repository/ServiciosRepository.cs
+++ UESAN.Jobs.Infrastructure/repository/ServiciosRepository.cs
@@ -6,10 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 using UESAN.proyecto.Infrastructure.Models;
 using UESAN.Proyecto.Core.entities;
+using UESAN.Proyecto.Core.InterfacesRepository;
 
 namespace UESAN.proyecto.Infrastructure.repository
 {
-	public class ServiciosRepository
+	public class ServiciosRepository : IServiciosRepository
 	{
 		readonly private OrdenesEventosContext _servicios;
 
@@ -20,7 +21,8 @@ namespace UESAN.proyecto.Infrastructure.repository
 		//GET ALL - TRAE TODOS LOS SERVICIOS
 		public async Task<IEnumerable<Servicios>> getAll()
 		{
-			var servicios = await _servicios.Servicios.ToListAsync();
+			var servicios = await _servicios.Servicios
+				.Include(y => y.IdEventoNavigation.IdUsuarioNavigation).ToListAsync();
 			if (servicios.Any())
 			{
 				return servicios;
@@ -34,8 +36,9 @@ namespace UESAN.proyecto.Infrastructure.repository
 		//Get By Id .
 		public async Task<Servicios> getById(int id)
 		{
-			var s = await _servicios.Servicios.Where(x => x.IdServicios == id).FirstOrDefaultAsync();
-			if(s == null)
+			var s = await _servicios.Servicios.Where(x => x.IdServicios == id)
+				.Include(y=>y.IdEventoNavigation.IdUsuarioNavigation).FirstOrDefaultAsync();	
+			if (s == null)
 			{
 				return null;
 			}
@@ -45,7 +48,8 @@ namespace UESAN.proyecto.Infrastructure.repository
 		//Trae todos los servicio dado el idEvento
 		public async Task<IEnumerable<Servicios>> getAllByIdEvento(int id)
 		{
-			var ser = await _servicios.Servicios.Where(x=> x.IdEventoNavigation.IdEvento == id).Include(y=> y.IdEventoNavigation).ToListAsync();
+			var ser = await _servicios.Servicios.Where(x => x.IdEventoNavigation.IdEvento == id)
+				.Include(y => y.IdEventoNavigation.IdUsuarioNavigation).ToListAsync();
 			if (ser.Any())
 			{
 				return ser;
@@ -56,7 +60,43 @@ namespace UESAN.proyecto.Infrastructure.repository
 			}
 		}
 
-		
+		//delete
+		public async Task<bool> delete(int id)
+		{
+			var eve = await _servicios.Servicios.Where(x => x.IdServicios == id).FirstOrDefaultAsync();
+			if (eve == null)
+			{
+				return false;
+			}
+			else
+			{
+				eve.Estado = "Eliminado";
+				int rows = await _servicios.SaveChangesAsync();
+				return rows > 0;
+			}
+		}
+
+		//modificar
+		public async Task<bool> update(Servicios e)
+		{
+			_servicios.Update(e);
+			int rows = _servicios.SaveChanges();
+			return rows > 0;
+		}
+
+		public async Task<int> insertServicio(Servicios servicios)
+		{
+			await _servicios.Servicios.AddAsync(servicios);
+			int rows = await _servicios.SaveChangesAsync();
+			if (rows > 0)
+			{
+				return servicios.IdServicios;
+			}
+			else
+			{
+				return 0;
+			}
+		}
 
 
 	}
