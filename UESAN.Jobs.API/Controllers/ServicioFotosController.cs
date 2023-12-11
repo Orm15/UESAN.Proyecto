41c187d6d299935282e2cab2fff9c49c7a7885f6
Christian <21200055@ue.edu.pe>
2023-12-11 00:06:43 -0500

    added 1 controller

---
+++ UESAN.Jobs.API/Controllers/ServicioFotosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using UESAN.Proyecto.Core.DTO;
+using UESAN.Proyecto.Core.entities;
+using UESAN.Proyecto.Core.InterfacesServices;
+
+namespace UESAN.proyecto.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicioFotosController : ControllerBase
+    {
+        private readonly IServicioFotosService _servicioFotosService;
+        public ServicioFotosController(IServicioFotosService servFotService)
+        {
+            _servicioFotosService = servFotService;
+        }
+
+        [HttpPost("CreateServicioFotos")]
+        public async Task<IActionResult> InsertServicioFotos(ServicioFotosDTO sfDTO)
+        {
+            var result = await _servicioFotosService.Create(sfDTO);
+            if (result == -1) return BadRequest(result);
+            else return Ok(result);
+        }
+
+        
+    }
+}
--- UESAN.Jobs.Core/DTO/ServicioFotosDTO.cs
+++ UESAN.Jobs.Core/DTO/ServicioFotosDTO.cs
@@ -22,21 +22,4 @@ namespace UESAN.Proyecto.Core.DTO
 
         public string? Link { get; set; }
     }
-
-    public class ServicioFotosCreateDTO
-    {
-        public int IdServicioFotos { get; set; }
-
-        public int? IdServicio { get; set; }
-
-        public string? CantidadFotos { get; set; }
-
-        public string? TipoFoto { get; set; }
-
-        public string? PesonaObjetivo { get; set; }
-
-        public string? Canales { get; set; }
-
-        public string? Link { get; set; }
-    }
 }
