ff8296908320dea7ca031c82d44d22b53b12a689
Victor Ormeño <20100835@ue.edu.pe>
2023-12-14 13:50:09 -0500

    Se agrego la edicion de video - solo la tabla

---
+++ OrdenesEventos/tablas/CircuitoCerrado.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[CircuitoCerrado]
+(
+	[IdCircuitoCerrado] INT NOT NULL PRIMARY KEY identity, 
+    [idServicio] INT NULL, 
+    [Guardar] NCHAR(10) NULL, 
+    [Link] NCHAR(100) NULL, 
+    [NumeroCamaras] NCHAR(10) NULL, 
+    [NumeroAngulos] NCHAR(10) NULL, 
+    [Angulos] NCHAR(100) NULL, 
+    [Estado] NCHAR(15) NULL , 
+     FOREIGN KEY ([idServicio]) REFERENCES [dbo].[Servicios] ([idServicios]),
+)
---
+++ OrdenesEventos/tablas/EscenasVideo.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[EscenasVideo]
+(
+	[idEscena] INT NOT NULL PRIMARY KEY identity, 
+    [idServicioEdicionVideo] INT NULL, 
+    [idVideo] INT NULL, 
+    [nombreVideo] NCHAR(50) NULL, 
+    [linkVideo] NCHAR(100) NULL, 
+    [nombreEscena] NCHAR(50) NULL, 
+    [DescripcionEscena] NCHAR(100) NULL, 
+    [tiempo] NCHAR(20) NULL, 
+    [Cambios] NCHAR(50) NULL, 
+    [estado] NCHAR(15) NULL,
+    FOREIGN KEY ([idServicioEdicionVideo]) REFERENCES [dbo].[ServicioEdicionVideo] ([idServicioEdicionVideo]),
+    FOREIGN KEY ([idVideo]) REFERENCES [dbo].[Videos] ([idVideo]),
+)
